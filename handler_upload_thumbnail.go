18b92f84ec20977d8b7c0f1cde79ac9ee0e89759
zdsdd <sebasadowy@gmail.com>
2025-01-03 17:58:40 +0100

    add thumbnails support

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
+	"strings"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
 )
+
+const maxMemory = 10 << 20
 
 func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Request) {
 	videoIDString := r.PathValue("videoID")
@@ -28,10 +32,68 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
+	fmt.Println("Uploading thumbnail for video", videoID, "by user", userID)
 
-	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
+	err = r.ParseMultipartForm(maxMemory)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error parsing multipart form", err)
+		return
+	}
 
-	// TODO: implement the upload here
+	file, head, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error forming file", err)
+		return
+	}
+	defer file.Close()
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	// Check Content-Type header
+	contentType := head.Header.Get("Content-Type")
+	if strings.HasPrefix(contentType, "image/") {
+		fmt.Println("File is an image:", contentType)
+	} else if strings.HasPrefix(contentType, "video/") {
+		fmt.Println("File is a video:", contentType)
+	} else {
+		respondWithError(w, http.StatusBadRequest, "Unsupported file type", nil)
+		return
+	}
+
+	// Optional: Inspect file bytes for robust type detection
+	imgData, err := io.ReadAll(file)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error when reading data from the file", err)
+		return
+	}
+
+	mimeType := http.DetectContentType(imgData)
+	fmt.Println("Detected MIME type:", mimeType)
+
+	// Validate MIME type (if required)
+	if !strings.HasPrefix(mimeType, "image/") && !strings.HasPrefix(mimeType, "video/") {
+		respondWithError(w, http.StatusBadRequest, "Unsupported file type", nil)
+		return
+	}
+	vd, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error fetching video from database", err)
+		return
+	}
+	if vd.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "User not authorized to upload thumbnail for this video", nil)
+		return
+	}
+	videoThumbnails[videoID] = thumbnail{
+		data:      imgData,
+		mediaType: mimeType,
+	}
+	var videoThumnnailUrl = fmt.Sprintf("http://localhost:%s/api/thumbnails/%s", cfg.port, videoID)
+	vd.ThumbnailURL = &videoThumnnailUrl
+
+	err = cfg.db.UpdateVideo(vd)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error updating video in database", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, vd)
 }
